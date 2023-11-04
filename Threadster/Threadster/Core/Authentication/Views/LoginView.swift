f1b684835c699294d21296b7132c0fc9cb288837
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-11-04 16:38:20 -0400

    Build UI for login view

---
+++ Threadster/Threadster/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,92 @@
+//
+//  LoginView.swift
+//  Threadster
+//
+//  Created by Jeffrey Sweeney on 11/4/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Image("threads-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                NavigationLink {
+                    Text("Forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing, 28)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .foregroundColor(.black)
+                }
+                
+                Button {
+                    print("Tapped LOGIN")
+                } label: {
+                    Text("Login")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Registration View")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                    .padding(.vertical, 16)
+                    .foregroundColor(.black)
+                }
+
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}

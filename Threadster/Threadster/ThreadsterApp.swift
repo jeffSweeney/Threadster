b8f7b1e19b46cf0ee96b7084bdccaf28ebed1d78
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-11-04 16:23:23 -0400

    Skeleton app init

---
+++ Threadster/Threadster/ThreadsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsterApp.swift
+//  Threadster
+//
+//  Created by Jeffrey Sweeney on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

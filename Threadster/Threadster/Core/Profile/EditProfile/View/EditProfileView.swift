8f3bc3c1a1e40202014f1ebc0702787386b36a9c
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-11-06 08:44:07 -0500

    Add the edit profile view

---
+++ Threadster/Threadster/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,98 @@
+//
+//  EditProfileView.swift
+//  Threadster
+//
+//  Created by Jeffrey Sweeney on 11/6/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
+    
+    var body: some View {
+        // Not actually navigating - but the free nav bar functionality is nice to have
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+                
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            
+                            Text("Jeff Sweeney")
+                        }
+                        
+                        Spacer()
+                        
+                        CircularImageProfileView(assetName: "CoolSky")
+                    }
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Enter your bio...", text: $bio, axis: .vertical)
+                    }
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Link")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Add link...", text: $link)
+                    }
+                    
+                    Divider()
+                    
+                    Toggle("Private profile", isOn: $isPrivateProfile)
+                    
+                    Divider()
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .overlay(content: {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                })
+                .padding()
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        print("TAPPED Cancel")
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        print("TAPPED Done")
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}

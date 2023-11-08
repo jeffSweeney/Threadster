57216e9520eff82fda1477c007f769ad622afe6f
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-11-08 17:41:49 -0500

    Add firebase integration into app: authentication, firestore; storage

--- Threadster/Threadster/App/ThreadsterApp.swift
+++ Threadster/Threadster/App/ThreadsterApp.swift
@@ -5,10 +5,22 @@
 //  Created by Jeffrey Sweeney on 11/4/23.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ThreadsterApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
